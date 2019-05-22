01a7564c6896cbfe33b0f95321457b34c168cf8f
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-05-22 18:02:22 -0400

    menu togg

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -18,3 +18,6 @@ const menuButton = document.querySelector('.menu-button');
 
 
 // Using your menuButton reference, add a click handler that calls toggleMenu
+
+menuButton.addEventListener.toggle('click', toggleMenu);
+
